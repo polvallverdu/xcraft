fa6a2d96cac9831138a023e1d278fcfed79e5981
polvallverdu <pvallverdualcaide@gmail.com>
2024-11-11 13:44:38 +0100

    Block drops multiplied

---
+++ common/src/main/java/dev/pov/xcraft/mixin/BlockDropMultiplierMixin.java
@@ -0,0 +1,29 @@
+package dev.pov.xcraft.mixin;
+
+import dev.pov.xcraft.Xcraft;
+import dev.pov.xcraft.config.ModConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(Block.class)
+public class BlockDropMultiplierMixin {
+
+    @Inject(method = "getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;", at = @At("RETURN"), cancellable = true)
+    private static void injectBlockDropMultiplier(BlockState state, ServerLevel level, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack tool, CallbackInfoReturnable<List<ItemStack>> cir) {
+        cir.getReturnValue().forEach(item -> item.setCount(item.getCount() * ModConfig.dropMultiplier));
+        System.out.println(ModConfig.dropMultiplier);
+        cir.setReturnValue(cir.getReturnValue());
+    }
+
+}
